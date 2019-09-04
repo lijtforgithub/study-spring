ae76a50c7b9c4b3877bfd076489619647c90bd45
Administrator <lijtforjava@sina.com>
2019-09-04 14:19:07 +0800

    RabbitMQ 私信转发

---
+++ src/main/java/com/ljt/study/pp/rabbitmq/delay/MessageConsumer.java
@@ -0,0 +1,32 @@
+package com.ljt.study.pp.rabbitmq.delay;
+
+import java.util.Objects;
+import java.util.concurrent.CountDownLatch;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+import org.springframework.stereotype.Component;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author LiJingTang
+ * @date 2019年9月4日 上午11:20:52
+ */
+@Slf4j
+@Component
+public class MessageConsumer implements MessageListener {
+    
+    static CountDownLatch cdLatch;
+
+    @Override
+    public void onMessage(Message message) {
+        String header = JSON.toJSONString(message.getMessageProperties());
+        log.debug("接收消息头信息：{}", header);
+        log.info("接收消息内容：{}", new String(message.getBody()));
+        
+        if (Objects.nonNull(cdLatch)) {
+            cdLatch.countDown();
+        }
+    }
+
+}
