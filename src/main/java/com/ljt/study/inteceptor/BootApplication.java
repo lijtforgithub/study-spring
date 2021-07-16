e5edc5b691b07ade8f2e02edc8062d7fe1f14363
李敬堂 <lijingtang@sanweiguoye.onaliyun.com>
2021-07-16 18:29:27 +0800

    Update BootApplication.java

--- src/main/java/com/ljt/study/inteceptor/BootApplication.java
+++ src/main/java/com/ljt/study/inteceptor/BootApplication.java
@@ -3,6 +3,7 @@ package com.ljt.study.inteceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 class BootApplication {
 
     public static void main(String[] args) {
