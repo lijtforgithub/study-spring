dd128cb5d3af8ac1dcd66bcb6e0f4a68d708bb47
lijtforgithub <lijtforjava@sina.com>
2022-03-10 21:14:24 +0800

    Update WebSocketConfig.java

--- src/main/java/com/ljt/study/protocol/WebSocketConfig.java
+++ src/main/java/com/ljt/study/protocol/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 /**
  * @author LiJingTang
@@ -14,9 +15,18 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    @Bean
+    ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(8192);
+        container.setMaxBinaryMessageBufferSize(8192);
+        return container;
+    }
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(wsTextHandler(), "/wsHandler")
+//                .setAllowedOrigins("*")
                 .addInterceptors(webSocketInterceptor());
     }
 
